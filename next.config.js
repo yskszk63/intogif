e6d5f289f4fe523b7aed250727b2de8bfb4ba074
yskszk63 <yskszk63@gmail.com>
2021-10-22 11:10:35 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
+}
